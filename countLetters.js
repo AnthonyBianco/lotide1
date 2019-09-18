ec6b7fb21d725246b72353ca23688fde142b25ad
AnthonyBianco <anthonysilvanobianco@gmail.com>
2019-09-18 20:54:36 +0000

    new file

---
+++ countLetters.js
@@ -0,0 +1,15 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    return console.log(`❌❌❌Assert Failed:  ${actual}  ===   ${expected}`);
+    }
+    return console.log(`✅✅✅ Assert Passed:  ${actual}  ===   ${expected}`);
+    
+  }
+
+  const countLetters = function(){
+    let countLetters = {
+    
+    };
+
+
+  }
