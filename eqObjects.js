4c7c2c3f361e9b06db9ffe0631a81bc47c112350
AnthonyBianco <anthonysilvanobianco@gmail.com>
2019-09-19 02:38:05 +0000

    new file

--- eqObjects.js
+++ eqObjects.js
@@ -1,39 +1,40 @@
 const assertEqual = function(actual, expected) {
   if (actual !== expected) {
     return console.log(`❌❌❌Assert Failed:  ${actual}  ===   ${expected}`);
-    }
-    return console.log(`✅✅✅ Assert Passed:  ${actual}  ===   ${expected}`);
-    
   }
-  const ab = { a: "1", b: "2" };
-  const ba = { b: "2", a: "1" };
-  const abc = { a: "1", b: "2", c: "3" };
+  return console.log(`✅✅✅ Assert Passed:  ${actual}  ===   ${expected}`);
+    
+};
 
-// const birds = {
-//   blue: 1,
-//   green: 2,
-//   yellow: 3,
-// }
-// const animals = {
-//   blue: 1,
-//   green: 2,
-//   yellow: 3,
-// }
+const eqArrays = function(actual, expected) {
+  if (actual.length !== expected.length) {
+    return false;
+  }
+  for (let i = 0; i < actual.length; i++) {
+    if (actual.indexOf(expected[i]) === -1) {
+      return false;
+    }
+  }
+  return true;
+};
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+const abc = { a: "1", b: "2", c: "3" };
 
 
 const eqObjects = function(object1, object2) {
-value1 = Object.keys(object1).length;
-value2 = Object.keys(object2).length;
-if (value1 !== value2){
+  const value1 = Object.keys(object1);
+  const value2 = Object.keys(object2);
+  if (!eqArrays(value1, value2)) {
     return false;
   } else {
-  for (let key in object1){
-  if (object1[key] !== object2[key]){
-  return false;
-  } 
+    for (let key in object1) {
+      if (object1[key] !== object2[key]) {
+        return false;
+      }
+    }
   }
   return true;
-  }
 };
 
 
