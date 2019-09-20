bda749a280664ea6199cbcd4df04337780c62030
AnthonyBianco <anthonysilvanobianco@gmail.com>
2019-09-20 01:27:47 +0000

    new file

---
+++ findKey.js
@@ -0,0 +1,45 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assert Passed:  ${actual}  ===   ${expected}`);
+    return true
+  } else {
+    console.log(`❌❌❌Assert Failed:  ${actual}  ===   ${expected}`);
+    return false
+  } 
+
+};
+
+const eqArrays = function(actual, expected){
+  if (actual.length !== expected.length){ 
+  return false;
+  } 
+  for (let i = 0; i < actual.length; i++){
+    if (actual[i] != expected[i]){
+      return false
+    }
+  }
+  return true
+}
+
+const findKey = function(object, callback){
+  const results = [];
+
+  for (let item in object) {
+    if (callback(object[item])){
+     return item;
+    }
+  }
+};
+
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 },
+}, x => x.stars === 2), "noma") // => "noma"
+
+// run over keys and then for each key run the callback
+
