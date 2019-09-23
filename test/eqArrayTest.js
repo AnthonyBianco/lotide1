b8d2739d48fdad9c82eb89995e4eb1887d150e2a
AnthonyBianco <anthonysilvanobianco@gmail.com>
2019-09-23 22:28:25 +0000

    Darren & Anthony

--- test/eqArrayTest.js
+++ test/eqArrayTest.js
@@ -1,5 +1,12 @@
 const eqArray = require('../eqArray');
-const assertEqual = require('../assertEqual')
+const assert = require('chai').assert;
+
+describe("#eqArray", () => {
+  it("returns truw is 1,2,3 = 1,2,3", () => {
+    assert.strictEqual(eqArray([1,2,3], [1,2,3]), true);
+  });
+});
 
 
-assertEqual(eqArray([1, 2, 3], [1, 2, 3]), true); // => should PASS
+
+//assertEqual(eqArray([1, 2, 3], [1, 2, 3]), true); // => should PASS
