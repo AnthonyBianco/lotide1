a9d2f85cb87e727707ac0b27923543d7f9019215
AnthonyBianco <anthonysilvanobianco@gmail.com>
2019-09-23 20:56:10 +0000

    Darren & Anthony

--- assertArrayEqual.js
+++ assertArrayEqual.js
@@ -1,26 +1,11 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assert Passed:  ${actual}  ===   ${expected}`);
-    return true
+const eqArray = require('./eqArray');
+
+const assertArrayEqual = function(actual, expected) {
+  if (eqArray(actual, expected)) {
+    console.log(`✅✅✅Assertion Passed:${actual} === ${expected}`);
   } else {
-    console.log(`❌❌❌Assert Failed:  ${actual}  ===   ${expected}`);
-    return false
-  } 
-
-};
-
-const eqArrays = function(actual, expected){
-  if (actual.length !== expected.length){ 
-  return false;
-  } 
-  for (let i = 0; i < actual.length; i++){
-    if (actual[i] != expected[i]){
-      return false
-    }
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !=== ${expected}`);
   }
-  return true
-}
+ }
 
-
-console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)); 
-console.log(assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false)); 
+module.exports = assertArrayEqual;
