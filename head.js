bb54497123841c6392746f207dc7caa507d4b517
AnthonyBianco <anthonysilvanobianco@gmail.com>
2019-09-23 20:20:09 +0000

    exporting modules

--- head.js
+++ head.js
@@ -1,13 +1,5 @@
 let head = function(parameter){
   return parameter[0];
-} 
+}; 
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assert Passed: " ${actual} " === "  ${expected}`);
-  } else {
-    console.log(`❌❌❌Assertion Failed: ${actual} " !== " ${expected}`);
-  }
-};
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
