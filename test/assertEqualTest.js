325c92e5d404029267c5f67841f8e06b4328389d
AnthonyBianco <anthonysilvanobianco@gmail.com>
2019-09-23 22:24:16 +0000

    Darren & Anthony

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,5 +1,14 @@
 const assertEqual = require('../assertEqual');
-
+const assert = require('chai').assert;
 assertEqual("Lighthouse labs","bootcamp");
 assertEqual(1,1);
 
+describe("#assertEqual", () => {
+  it("returns false if Lighthouse labs != bootcamp", () => {
+    assert.strictEqual(assertEqual("Lighthouse labs", "bootcamp"));
+  });
+});
+
+it("returns true if 1 equals 1", () => {
+  assert.strictEqual(assertEqual('1', '1')); 
+});
